f7ff9bc3d12b207720fda6b1705526133b105c38
hemanthVk <hemanthkumarvk98@gmail.com>
2023-09-05 22:26:51 +0530

    added

--- tools.js
+++ tools.js
@@ -3,10 +3,13 @@ let optionsCont = document.querySelector(".options-cont");
 let optionsFlag = true;
 let pencilToolCont = document.querySelector(".pencil-tool-cont");
 let eraserToolCont = document.querySelector(".eraser-tool-cont");
+let pencil = document.querySelector(".pencil");
+let eraser = document.querySelector(".eraser");
+let pencilFlag = false;
+let eraserFlag = false;
 
 //true false to hide tools
 optionsCont.addEventListener("click", (e) => {
-  console.log(e);
   optionsFlag = !optionsFlag;
   if (optionsFlag) openTools();
   else closeTools();
@@ -25,3 +28,13 @@ function closeTools() {
   pencilToolCont.style.display = "none";
   eraserToolCont.style.display = "none";
 }
+pencil.addEventListener("click", (e) => {
+  pencilFlag = !pencilFlag;
+  if (pencilFlag) pencilToolCont.style.display = "block";
+  else pencilToolCont.style.display = "none";
+});
+eraser.addEventListener("click", (e) => {
+  eraserFlag = !eraserFlag;
+  if (eraserFlag) eraserToolCont.style.display = "block";
+  else eraserToolCont.style.display = "none";
+});
