638f7b846647e099db672c8adb82bea47ae63f5c
hemanthVk <hemanthkumarvk98@gmail.com>
2023-09-05 23:43:55 +0530

    sticky bar

--- tools.js
+++ tools.js
@@ -5,6 +5,7 @@ let pencilToolCont = document.querySelector(".pencil-tool-cont");
 let eraserToolCont = document.querySelector(".eraser-tool-cont");
 let pencil = document.querySelector(".pencil");
 let eraser = document.querySelector(".eraser");
+let sticky = document.querySelector(".sticky");
 let pencilFlag = false;
 let eraserFlag = false;
 
@@ -38,3 +39,55 @@ eraser.addEventListener("click", (e) => {
   if (eraserFlag) eraserToolCont.style.display = "block";
   else eraserToolCont.style.display = "none";
 });
+sticky.addEventListener("click", (e) => {
+  let stickyCont = document.createElement("div");
+  stickyCont.setAttribute("class", "sticky-cont");
+  stickyCont.innerHTML = `
+  <div class="header-cont">
+        <div class="minimize"></div>
+        <div class="remove"></div>
+      </div>
+      <div class="note-cont">
+        <textarea></textarea>
+      </div>
+  `;
+  document.body.appendChild(stickyCont);
+
+  stickyCont.onmousedown = function (event) {
+    dragAndDrop(stickyCont, event);
+  };
+
+  stickyCont.ondragstart = function () {
+    return false;
+  };
+});
+function dragAndDrop(element, event) {
+  let shiftX = event.clientX - element.getBoundingClientRect().left;
+  let shiftY = event.clientY - element.getBoundingClientRect().top;
+
+  element.style.position = "absolute";
+  element.style.zIndex = 1000;
+  document.body.append(element);
+
+  moveAt(event.pageX, event.pageY);
+
+  // moves the ball at (pageX, pageY) coordinates
+  // taking initial shifts into account
+  function moveAt(pageX, pageY) {
+    element.style.left = pageX - shiftX + "px";
+    element.style.top = pageY - shiftY + "px";
+  }
+
+  function onMouseMove(event) {
+    moveAt(event.pageX, event.pageY);
+  }
+
+  // move the ball on mousemove
+  document.addEventListener("mousemove", onMouseMove);
+
+  // drop the ball, remove unneeded handlers
+  element.onmouseup = function () {
+    document.removeEventListener("mousemove", onMouseMove);
+    element.onmouseup = null;
+  };
+}
